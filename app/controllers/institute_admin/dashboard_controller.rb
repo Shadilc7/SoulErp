030b470648bef68d4afa30248db534754819ae1e
shadil <shadilc7@gmail.com>
2025-02-09 21:02:05 +0530

    feat: institution admin view pages

--- app/controllers/institute_admin/dashboard_controller.rb
+++ app/controllers/institute_admin/dashboard_controller.rb
@@ -2,8 +2,8 @@ module InstituteAdmin
   class DashboardController < InstituteAdmin::BaseController
     def index
       @sections_count = current_institute.sections.count
-      @participants_count = current_institute.users.participant.count
-      @trainers_count = current_institute.users.trainer.count
+      @participants_count = current_institute.participants.joins(:user).count
+      @trainers_count = current_institute.trainers.joins(:user).count
     end
   end
 end
